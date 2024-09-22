c65301affaa565ca52b429a78ad85db091fa613c
DharaniDJ <nvchinta@asu.edu>
2024-09-22 16:29:40 -0500

    added api gateway microservice

--- payment-service/src/main/java/com/example/ps/api/service/PaymentService.java
+++ payment-service/src/main/java/com/example/ps/api/service/PaymentService.java
@@ -1,13 +1,13 @@
 package com.example.ps.api.service;
 
-import com.example.ps.api.repository.PaymentRepository;
-import com.example.ps.api.entity.Payment;
-
 import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.ps.api.entity.Payment;
+import com.example.ps.api.repository.PaymentRepository;
 
 @Service
 public class PaymentService {
@@ -20,9 +20,14 @@ public class PaymentService {
         payment.setTransactionId(UUID.randomUUID().toString());
         return repository.save(payment);
     }
-    
+
     public String paymentProcessing(){
         // api should be 3rd party payment gateway (like paypal may be)
         return new Random().nextBoolean() ? "success" : "false";
     }
+
+    public Payment findPaymentHistoryByOrderId(int orderId) {
+        return repository.findByOrderId(orderId);
+    }
+
 }
