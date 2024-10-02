a81011d232c71dec7c263fb131f2e0d0c7ce1333
DharaniDJ <dharani56525@gmail.com>
2024-10-02 12:39:36 -0500

    Added logging into order and payment microservices

--- order-service/src/main/java/com/example/os/api/service/OrderService.java
+++ order-service/src/main/java/com/example/os/api/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.os.api.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -12,6 +14,8 @@ import com.example.os.api.common.TransactionRequest;
 import com.example.os.api.common.TransactionResponse;
 import com.example.os.api.entity.Order;
 import com.example.os.api.repository.OrderRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 @RefreshScope
@@ -27,15 +31,21 @@ public class OrderService {
     @Value("${microservices.payment-service.endpoints.endpoint.url}")
     private String ENDPOINT_URL;
 
-    public TransactionResponse saveOrder(TransactionRequest request) {
+    private Logger log = LoggerFactory.getLogger(OrderService.class);
+
+    public TransactionResponse saveOrder(TransactionRequest request) throws JsonProcessingException {
         String response = "";
 
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
+
+        log.info("OrderService Request : {}", new ObjectMapper().writeValueAsString(request));
         // rest call
         Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
+
+        log.info("PaymentService Response from OrderService Rest call : {}", new ObjectMapper().writeValueAsString(paymentResponse));
 
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api , order added to cart";
 
--- payment-service/src/main/java/com/example/ps/api/service/PaymentService.java
+++ payment-service/src/main/java/com/example/ps/api/service/PaymentService.java
@@ -3,11 +3,15 @@ package com.example.ps.api.service;
 import java.util.Random;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.ps.api.entity.Payment;
 import com.example.ps.api.repository.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentService {
@@ -15,9 +19,12 @@ public class PaymentService {
     @Autowired
     private PaymentRepository repository;
 
-    public Payment doPayment(Payment payment) {
+    private Logger log = LoggerFactory.getLogger(PaymentService.class);
+
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
         payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString());
+        log.info("PaymentService Request : {}", new ObjectMapper().writeValueAsString(payment));
         return repository.save(payment);
     }
 
@@ -26,8 +33,10 @@ public class PaymentService {
         return new Random().nextBoolean() ? "success" : "false";
     }
 
-    public Payment findPaymentHistoryByOrderId(int orderId) {
-        return repository.findByOrderId(orderId);
+    public Payment findPaymentHistoryByOrderId(int orderId) throws JsonProcessingException {
+        Payment payment = repository.findByOrderId(orderId);
+        log.info("PaymentService findPaymentHistoryByOrderId : {}", new ObjectMapper().writeValueAsString(payment));
+        return payment;
     }
 
 }
