3ff611fbc73da54c1c5a119765fcb4c63117cde7
DharaniDJ <dharani56525@gmail.com>
2024-10-05 15:59:50 -0500

    added resilience4j for circuit breaker in order microservice

--- order-service/src/main/java/com/example/os/api/controller/OrderController.java
+++ order-service/src/main/java/com/example/os/api/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.example.os.api.controller;
 
 import com.example.os.api.service.OrderService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import com.example.os.api.common.TransactionRequest;
 import com.example.os.api.common.TransactionResponse;
 
@@ -18,10 +21,18 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
+    public static final String PAYMENT_SERVICE = "paymentService";
+
     @PostMapping("/bookOrder")
+    @CircuitBreaker(name = PAYMENT_SERVICE, fallbackMethod = "bookOrderFallback")
     public TransactionResponse bookOrder(@RequestBody TransactionRequest request) throws JsonProcessingException{
         return service.saveOrder(request);
 
         // do a rest call to payment and pass the order id
+    }
+
+    // Fallback method for bookOrder
+    public TransactionResponse bookOrderFallback(TransactionRequest request, Throwable throwable) {
+        return new TransactionResponse(null, -1.0, Integer.toString(-1),"Fallback response: Payment service is currently unavailable.");
     }
 }
